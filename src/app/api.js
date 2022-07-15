cc47fd9d9e0a931cd34cdb7a1f19ce7f9ce6ce94
Ruby Sih <ruby.sih@skylinetw.com>
2022-07-15 12:26:30 +0800

    refactor: api

--- src/app/api.js
+++ src/app/api.js
@@ -4,47 +4,23 @@ import { ServerUrl, StatusCode } from '../constants'
 const folder = 'contents'
 const infoFile = 'structure.json'
 
-export const loadMenu_ = () =>
+const get = url =>
   axios
-    .get(`${ServerUrl}/menu`)
+    .get(url)
     .then(({ data, status, statusText }) => {
       if (status === StatusCode.OK) return data
       throw new Error(`${status}: ${statusText}`)
     })
     .catch(e => console.log('api err', e))
 
-export const loadContent = (subPath, lang) =>
-  axios
-    .get(`contents/${subPath.join('/')}/README_${lang}.md`)
-    .then(({ data, status, statusText }) => {
-      if (status === StatusCode.OK) return data
-      throw new Error(`${status}: ${statusText}`)
-    })
-    .catch(e => console.log('e', e))
+export const loadMenu_ = () => get(`${ServerUrl}/menu`)
 
-export const loadMenu = () =>
-  axios
-    .get(`${folder}/${infoFile}`)
-    .then(({ data, status, statusText }) => {
-      if (status === StatusCode.OK) return data
-      throw new Error(`${status}: ${statusText}`)
-    })
-    .catch(e => console.log('e', e))
+export const loadContent = (subPath, lang) =>
+  get(`contents/${subPath.join('/')}/README_${lang}.md`)
+
+export const loadMenu = () => get(`${folder}/${infoFile}`)
 
 export const loadSubMenu = subPath =>
-  axios
-    .get(`${folder}/${subPath.join('/')}/${infoFile}`)
-    .then(({ data, status, statusText }) => {
-      if (status === StatusCode.OK) return data
-      throw new Error(`${status}: ${statusText}`)
-    })
-    .catch(e => console.log('e', e))
+  get(`${folder}/${subPath.join('/')}/${infoFile}`)
 
-export const loadI18n = () =>
-  axios
-    .get(`${folder}/i18n.json`)
-    .then(({ data, status, statusText }) => {
-      if (status === StatusCode.OK) return data
-      throw new Error(`${status}: ${statusText}`)
-    })
-    .catch(e => console.log('e', e))
+export const loadI18n = () => get(`${folder}/i18n.json`)
